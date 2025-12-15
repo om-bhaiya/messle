b841e7ba32e71f06841409df6e6149139e83ce87
Om Bhaiya <ombhaiya18@gmail.com>
2025-12-15 15:40:49 +0530

    Removed import which was added earlier to add fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MessDetailPage from "./pages/MessDetailPage";
-import ImportPage from "./pages/ImportPage";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/mess/:messId" element={<MessDetailPage />} />
-        <Route path="/import" element={<ImportPage />} />
       </Routes>
     </Router>
   );
